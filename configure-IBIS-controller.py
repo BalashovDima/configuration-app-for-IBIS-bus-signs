a01f1ec5354d052fd3961256813c6b2159c5d42c
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-08-23 15:43:21 +0300

    Add UI window using customtkinter

--- configure-IBIS-controller.py
+++ configure-IBIS-controller.py
@@ -1,2 +1,12 @@
+import customtkinter
 from modules.generate_zM_sending_code import generate_zM_sending_code
 from modules.generate_zM_LCD_text_code import generate_zM_LCD_text_code
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+app = customtkinter.CTk()
+app.geometry("700x500")
+app.title("Configure IBIS controller")
+
+app.mainloop()
