78ca18206d8b5716998e46ea3ef5d5f3c711704e
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-08-26 11:42:57 +0300

    Add bottom bar
    Bottom bar lets user choose file with data for configuration (and save it) and arduino file to modify/flash

--- ui/app.py
+++ ui/app.py
@@ -1,6 +1,7 @@
 import json
 import customtkinter as ctk
 from .zM_input import zM_input
+from .bottom_bar import Bottom_bar
 
 class App(ctk.CTk):
     def __init__(self):
@@ -14,11 +15,13 @@ class App(ctk.CTk):
             # self.data = json.loads(string)
 
         self.zM_frame = ctk.CTkScrollableFrame(self)
+        self.bottom_bar = Bottom_bar(self)
 
         for index, item in enumerate(self.data['zM_texts']):
             zM_input_frame = zM_input(self.zM_frame, item, index)
             zM_input_frame.pack(fill='x', padx=10, pady=5)
 
         self.zM_frame.pack(fill='both', expand=True)
+        self.bottom_bar.pack(fill='both')
             
 
---
+++ ui/bottom_bar.py
@@ -0,0 +1,121 @@
+import customtkinter as ctk
+from customtkinter import filedialog
+import os
+
+class Bottom_bar(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent, 
+                         fg_color='#333232',
+                         height=100,
+                         corner_radius=0)
+    
+        self.data_file_frame = Data_file_frame(self)
+        self.arduino_file_frame = Arduino_file_frame(self)
+
+        self.data_file_frame.pack(fill='both', pady=(10,0), padx=10)
+        self.arduino_file_frame.pack(fill='both', pady=(10,10), padx=10)
+
+class Data_file_frame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent,
+                         fg_color='transparent',
+                         corner_radius=5)
+        
+        # tk variables
+        self.data_file_path_var = ctk.StringVar()
+
+        # frames
+        self.labels_frame = ctk.CTkFrame(self,
+                                         fg_color='transparent',
+                                         corner_radius=5)
+        self.buttons_frame = ctk.CTkFrame(self, fg_color='transparent')
+
+        # widgets
+        self.label = ctk.CTkLabel(self.labels_frame, 
+                                  text='Data file:', 
+                                  font=('Regular', 14, 'bold'))
+                                  
+        self.path_entry = ctk.CTkEntry(self.labels_frame, 
+                                       font=('Regular', 14, 'bold'), 
+                                       textvariable=self.data_file_path_var)
+        
+        self.explore_button = ctk.CTkButton(self.buttons_frame, 
+                                            text='Browse', 
+                                            command=self.browse_files,
+                                            width=40)
+        
+        self.save_button = ctk.CTkButton(self.buttons_frame, text='Save', width=35)
+
+        # layout
+        self.columnconfigure(1, weight=1, uniform='a')
+        self.columnconfigure(0, weight=5, uniform='a')
+        self.labels_frame.grid(row=0, column=0, sticky='nesw')
+        self.buttons_frame.grid(row=0, column=1, sticky='e')
+
+        self.label.pack(side='left', padx = 5)
+        self.path_entry.pack(side='left', expand=True, fill='both')
+
+        self.save_button.pack(side='right', padx=0)
+        self.explore_button.pack(side='right', padx=5)     
+
+    def browse_files(self):
+        self.filename = filedialog.askopenfilename(initialdir = os.path.dirname(__file__)+'/../',
+                                                   title = "Select a File",
+                                                   filetypes = (("json files","*.json*"),("all files", "*.*")))
+
+        self.data_file_path_var.set(self.filename)
+        self.path_entry.xview('end')
+       
+
+class Arduino_file_frame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent,
+                         fg_color='transparent',
+                         corner_radius=5)
+        
+        # tk variables
+        self.data_file_path_var = ctk.StringVar()
+
+        # frames
+        self.labels_frame = ctk.CTkFrame(self,
+                                         fg_color='transparent',
+                                         corner_radius=5)
+        self.buttons_frame = ctk.CTkFrame(self, fg_color='transparent')
+        
+        # widgets
+        self.label = ctk.CTkLabel(self.labels_frame, 
+                                  text='Arduino file:', 
+                                  font=('Regular', 14, 'bold'))
+                                  
+        self.path_entry = ctk.CTkEntry(self.labels_frame, 
+                                       font=('Regular', 14, 'bold'), 
+                                       textvariable=self.data_file_path_var)
+        
+        self.explore_button = ctk.CTkButton(self.buttons_frame, 
+                                            text='Browse', 
+                                            command=self.browse_files,
+                                            width=40)
+        
+        self.modify_button = ctk.CTkButton(self.buttons_frame, text='Modify', width=35)
+        self.flash_button = ctk.CTkButton(self.buttons_frame, text='Flash', width=35)
+
+        # layout
+        self.columnconfigure(1, weight=2, uniform='a')
+        self.columnconfigure(0, weight=5, uniform='a')
+        self.labels_frame.grid(row=0, column=0, sticky='nesw')
+        self.buttons_frame.grid(row=0, column=1, sticky='e')
+
+        self.label.pack(side='left', padx = 5)
+        self.path_entry.pack(side='left', expand=True, fill='both')
+
+        self.flash_button.pack(side='right', padx=0)
+        self.modify_button.pack(side='right', padx=5)
+        self.explore_button.pack(side='right', padx=0)     
+
+    def browse_files(self):
+        self.filename = filedialog.askopenfilename(initialdir = os.path.dirname(__file__)+'/../',
+                                                   title = "Select a File",
+                                                   filetypes = (("arduino files","*.ino*"),("all files", "*.*")))
+
+        self.data_file_path_var.set(self.filename)
+        self.path_entry.xview('end')
