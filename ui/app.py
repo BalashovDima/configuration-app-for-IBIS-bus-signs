84e46bed29fbce9a1ce1fc58e77d9a88e00b7102
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-08-26 12:54:24 +0300

    Add rendering of a data file the users chooses

--- ui/app.py
+++ ui/app.py
@@ -10,18 +10,31 @@ class App(ctk.CTk):
         self.geometry("700x500")
         self.title("Configure IBIS controller")
 
-        with open('data.json', 'r',  encoding='utf-8') as file:
-            self.data = json.load(file)
-            # self.data = json.loads(string)
+        self.data_file = ''
 
         self.zM_frame = ctk.CTkScrollableFrame(self)
         self.bottom_bar = Bottom_bar(self)
+
+        self.bottom_bar.data_file_frame.explore_button.bind('<Button-1>', self.rerender_zM, add='+')
+      
+
+        self.zM_frame.pack(fill='both', expand=True)
+        self.bottom_bar.pack(fill='both')
+
+    def rerender_zM(self, event):
+        self.data_file = self.bottom_bar.data_file_frame.data_file_path_var.get()
+
+        if self.data_file == '': return
+
+        for widgets in self.zM_frame.winfo_children():
+            widgets.destroy()
+
+        with open(self.data_file, 'r',  encoding='utf-8') as file:
+            self.data = json.load(file)
 
         for index, item in enumerate(self.data['zM_texts']):
             zM_input_frame = zM_input(self.zM_frame, item, index)
             zM_input_frame.pack(fill='x', padx=10, pady=5)
 
-        self.zM_frame.pack(fill='both', expand=True)
-        self.bottom_bar.pack(fill='both')
             
 
