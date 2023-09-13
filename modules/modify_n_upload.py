598d1cdbcd1c5c3b6bbf06ee2a80f463ae310e70
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-09-13 23:04:22 +0300

    Change upload to run in background to keep ui live
    Now uploading is done asynchronously, UI stays live (doesn't freeze) and shows that uploading is in progress. The output is more detailed (-v flag (verbose))

--- modules/modify_n_upload.py
+++ modules/modify_n_upload.py
@@ -1,23 +1,54 @@
 import subprocess
+import threading
+import time
+import customtkinter as ctk
 from .modify_arduino_file import modify_arduino_file
 
-def modify_n_upload(file_path, data, com, fqbn):
+'''
+Modifies specified arduino sketch and uploads it (compilation is done automaticaly)
+
+Return a list of lines that are the output from arduino-cli
+
+Arguments: file_path -- path to arduino file; data -- data object with all the information needed to modify the sketch; com -- com port in format 'COM<number>'; fqbn -- fqbn of the board connected to the port; inprogress_label_var -- label that is used to indicate that upload is in progress; window -- upload window ctkinter object, it is needed to keep the ui updated (not frozen)
+'''
+def modify_n_upload(file_path, data, com, fqbn, inprogress_label_var, window):
     modify_arduino_file(file_path, data)
 
-    output = ''
+    upload_command = f"arduino-cli upload -p {com} --fqbn {fqbn} {file_path} -v"
+    upload_process = subprocess.Popen(upload_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    compile_output = subprocess.run(
-        ["arduino-cli", "compile", "--fqbn", fqbn, file_path],
-        capture_output=True,
-        text=True
-    ).stdout
-    output += ''.join(compile_output.splitlines()[:2]) + '\n\n'
+    output = []
 
-    upload_output = subprocess.run(
-        ["arduino-cli", "upload", "-p", com, "--fqbn", fqbn, file_path],
-        capture_output=True,
-        text=True
-    ).stdout
-    output += upload_output
+    def read_upload_output(output):
+        for line in upload_process.stdout:
+            print(f"Upload: {line.strip()}")
+            output.append(line)
+        for line in upload_process.stderr:
+            print(f"Upload Error: {line.strip()}")
+            output.append(line)
+
+    # Start a thread to read and print the upload output and errors
+    upload_output_thread = threading.Thread(target=read_upload_output, args=[output])
+    upload_output_thread.start()
+
+    # Periodically print dots to indicate ongoing process
+    counter = 0
+    while upload_process.poll() is None:
+        print(".", end="", flush=True)
+        text = ''
+        if counter == 0:
+            text = '⦿'
+        elif counter == 1:
+            text = '⦿⦿'
+        elif counter == 2:
+            text = '⦿⦿⦿'
+        counter = (counter + 1) % 3
+        inprogress_label_var.set(text)
+        window.update()
+        time.sleep(0.3)
+    inprogress_label_var.set('')
+
+    # Wait for the output threads to finish
+    upload_output_thread.join()
 
     return output
--- ui/upload_window.py
+++ ui/upload_window.py
@@ -6,7 +6,7 @@ class Upload_window(ctk.CTkToplevel):
     def __init__(self, parent, data, arduino_file):
         super().__init__(parent)
 
-        self.geometry('400x300')
+        self.geometry('520x370')
         self.resizable(False, False)
         self.title('Upload to Arduino')
 
@@ -26,10 +26,14 @@ class Upload_window(ctk.CTkToplevel):
                                               values=['Arduino Nano (328)', 
                                                       'Arduino Nano (328 old bootloader)', 
                                                       'Arduino Uno'], 
-                                              variable=self.board_var)
+                                              variable=self.board_var,
+                                              width=200)
         self.board_var.set('Arduino Nano (328)')
         self.board_label.pack(side='left', padx=5)
         self.board_combobox.pack(side='left')
+
+        # event handling
+        self.board_var.trace_add("write", self.update_fqbn)
 
         # com port
         self.com_frame = ctk.CTkFrame(self)
@@ -41,22 +45,29 @@ class Upload_window(ctk.CTkToplevel):
         # button
         self.upload_button = ctk.CTkButton(self, text='Upload', command=self.upload)
 
-        self.output = ctk.CTkLabel(self, wraplength=390, textvariable=self.output_var)
+        # output
+        self.output = ctk.CTkTextbox(self)
+        # in progress indicator
+        self.inprogress_var = ctk.StringVar(value='')
+        self.inprogress_label = ctk.CTkLabel(self, textvariable = self.inprogress_var, font=('Calibri', 20))
+        self.inprogress_label.place(x=10, y=5)
+
         # layout
         self.board_frame.pack(pady=10)
         self.com_frame.pack()
         self.upload_button.pack(pady=10)
         self.output.pack(expand=True, fill='both')
 
-    def update_fqbn(self):
-        if(self.board_var == 'Arduino Nano'):
+    def update_fqbn(self, *args):
+        if(self.board_var.get() == 'Arduino Nano (328)'):
             self.fqbn = 'arduino:avr:nano:cpu=atmega328'
-        elif(self.board_var == 'Arduino Nano (old bootloader)'):
+        elif(self.board_var.get() == 'Arduino Nano (328 old bootloader)'):
             self.fqbn = 'arduino:avr:nano:cpu=atmega328old'
-        elif(self.board_var == 'Arduino Uno'):
+        elif(self.board_var.get() == 'Arduino Uno'):
             self.fqbn = 'arduino:avr:uno'
 
     def upload(self):
+        #get com port number using regular expression
         pattern = re.compile(r'^com(\d)$', re.IGNORECASE)
         match = pattern.match(self.com_var.get())
 
@@ -70,5 +81,11 @@ class Upload_window(ctk.CTkToplevel):
             self.output_var.set('Invalid COM port')
             return
         
+        self.output.delete(0.0, ctk.END)
+        self.upload_button.configure(state="disabled")
 
-        self.output_var.set(modify_n_upload(self.arduino_file, self.data, com, self.fqbn))
+        # upload and save the output, then show the output
+        uploading_output = modify_n_upload(file_path=self.arduino_file, data=self.data, com=com, fqbn=self.fqbn, inprogress_label_var = self.inprogress_var, window=self)
+        self.output.insert(ctk.END, ''.join(uploading_output))
+
+        self.upload_button.configure(state="enabled")
